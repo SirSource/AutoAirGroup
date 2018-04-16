c9182a4b04939456676b182436148d6e76f91b5c
jcdfusion <jaime.cortes@upr.edu>
2018-04-15 22:01:56 -0400

    Added templates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from handler.users import UserHandler
 from config.dbconfig import client
 
@@ -6,8 +6,20 @@ app = Flask(__name__)
 #mongo = PyMongo(app)
 
 @app.route('/')
-def greeting():
-    return '<h1>Auto Air Group</h1>'
+def home():
+    return render_template('index.html')
+
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/accounts')
+def accounts():
+    return render_template('userSession.html')
 
 @app.route('/users')
 def getAllUsers():
