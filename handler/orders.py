219106c5cbf917fc143ea032bc4d4e2ff471fa54
jcdfusion <jaime.cortes@upr.edu>
2018-04-24 14:02:38 -0400

    Orders

--- handler/orders.py
+++ handler/orders.py
@@ -1,6 +1,5 @@
 from dao.orders import OrdersDao
 
-
 class OrdersHandler:
 
     def order_dictionary(self, row):
@@ -25,3 +24,54 @@ class OrdersHandler:
         if item == None:
             return None
         return self.order_dictionary(item)
+
+    def getOrdersByEmail(self, email):
+        dao = OrdersDao()
+        item = dao.getOrdersByEmail(email)
+        if item == None:
+            return None
+        return self.order_dictionary(item)
+
+    def getOrdersByStatus(self, status):
+        dao = OrdersDao()
+        item = dao.getOrdersByStatus(status)
+        if item == None:
+            return None
+        return self.order_dictionary(item)
+
+    def getOrdersShipped(self):
+        dao = OrdersDao()
+        item = dao.getOrdersShipped()
+        if item == None:
+            return None
+        return self.order_dictionary(item)
+
+    def deleteOrderById(self, oid):
+        if self.orderExists(oid):
+            OrdersDao().deleteOrderById(oid)
+            return True
+        else:
+            return False
+
+    def deleteOrderByUserEmail(self, email):
+        if self.emailOrderExists(email):
+            OrdersDao().deleteOrderByUserEmail(email)
+            return True
+        else:
+            return False
+
+    #---Auxiliary Methods---#
+
+    def orderExists(self, oid):
+        order = OrdersDao().getOrdersByOrderID(oid)
+        if order == None:
+            return False
+        else:
+            return True
+
+    def emailOrderExists(self, email):
+        order = OrdersDao().getOrdersByEmail(email)
+        if order == None:
+            return False
+        else:
+            return True
