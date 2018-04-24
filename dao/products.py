7f8697381e4eaac393e17f8a8a9b8d1bdfdff66c
luisroperez <luis.perez67@upr.edu>
2018-04-24 19:30:31 -0400

    Update products.py

--- dao/products.py
+++ dao/products.py
@@ -1,198 +1,230 @@
-from dao.products import ProductsDao
+from config.dbconfig import client
 
-class ProductsHandler:
 
-    def products_dictionary(self, row):
-        car = row['car']
-        del row['car']
-        del row['_id']
-        result = {**car, **row}
-        return result
+class ProductsDao:
 
-    def product_id_dictionary(self, row):
-        pid = row['pid']
-        result = {*pid, *row}
-        return result
-
-    def product_carInfo_dictionary(self, row):
-        car = row['car']
-        del row['_id']
-        result = {**car, **row}
-        return result
-
-    # =========================================================#
+    def __init__(self):
+        self.db = client.AutoAirGroupdb.products
 
     def getAllProducts(self):
-        """
-        Returns all the products available in the database
-        :return:
-        TESTED: YES: WORKS
-        """
-        dao = ProductsDao()
-        list = dao.getAllProducts()
-        result_list = []
-        for row in list:
-            result = self.products_dictionary(row)
-            result_list.append(result)
-        return result_list
+        allProducts = []
+        products = self.db
+        for doc in products.find():
+            allProducts.append(doc)
+        return allProducts
 
-    # Returns the ID of the product by entering the name of the product
-    # author: Luis Perez
-    #TESTED: YES; IT WORKS
-    def getProductIDbyName(self, pname):
-        dao = ProductsDao()
-        list = dao.getProductIDbyName(pname)
-        result_list = []
-        for row in list:
-            result_list.append(row)
-        return result_list
+    def getAllProductsByCategory(self, category):
+        allProducts = []
+        products = self.db
+        for doc in products.find({"pcategory": category}):
+            allProducts.append(doc)
+        return allProducts
 
+    def getAllProductsByBrand(self, brand):
+        allProducts = []
+        products = self.db
+        for doc in products.find({"pbrand": brand}):
+            allProducts.append(doc)
+        return allProducts
 
-    def getProductInfoByProductName(self,pname):
+    def getProductByID(self, pid):
+        products = self.db
+        p = []
+        for doc in products.find({"pid": pid}):
+            p.append(doc)
+        return p
+
+    def getProductByName(self, pname):
         """
-        Returns Information of product by searching its product name
+
         :param pname:
         :return:
-        TESTED: YES: IT WORKS
         """
-        dao = ProductsDao()
-        list = dao.getProductByName(pname)
-        result_list = []
-        for row in list:
-            result = self.products_dictionary(row)
-            result_list.append(result)
-        return result_list
+        products = self.db
+        p = []
+        for doc in products.find({"pname": pname}):
+            p.append(doc)
+        return p
 
-
-    # returns all the car make available in the db
+    #Returns the product ID based on product name
     # author: Luis Perez
-    # TESTED: YES WORKS
-    def getAllCarMake(self):
-        dao = ProductsDao()
-        list = dao.getCarMake()
-        result_list = []
-        for row in list:
-            result_list.append(row)
-        return result_list
+    # tested:YES
+    def getProductIDbyName(self, pname):
+        products = self.db
+        pid = []
+        for doc in products.find({'pname': pname}, {'pid': 1, '_id': 0}):
+            pid.append(doc)
+        return pid
 
-    # returns all the car model available in the db
+    # Returns all the car make available in the db
     # author: Luis Perez
-    # TESTED: YES WORKS
-    def getAllCarModel(self):
-        dao = ProductsDao()
-        list = dao.getCarModel()
-        result_list = []
-        for row in list:
-            result_list.append(row)
-        return result_list
+    # tested:YES
+    def getCarMake(self):
+        products = self.db
+        cm = []
+        for doc in products.distinct('car.make'):
+            cm.append(doc)
+        return cm
 
-    # returns all the car year available in the db
+    # Returns all the car model available in the db
     # author: Luis Perez
-    # TESTED: YES WORKS
-    def getAllCarYear(self):
-        dao = ProductsDao()
-        list = dao.getCarYear()
-        result_list = []
-        for row in list:
-            result_list.append(row)
-        return result_list
+    # tested:YES
+    def getCarModel(self):
+        products = self.db
+        cm = []
+        for doc in products.distinct('car.model'):
+            cm.append(doc)
+        return cm
 
-    # returns all the car motor available in the db
+    # Returns all the car years available in the db
     # author: Luis Perez
-    #TESTED: YES WORKS
-    def getAllCarMotor(self):
-        dao = ProductsDao()
-        list = dao.getCarMotor()
-        result_list = []
-        for row in list:
-            result_list.append(row)
-        return result_list
+    # tested:YES
+    def getCarYear(self):
+        products = self.db
+        cy = []
+        for doc in products.distinct('car.year'):
+            cy.append(str(doc))
+        return cy
 
-    # Returns product info by car make, car model, car year, car motor
+    # Returns all the car motors available in the db
     # author: Luis Perez
-    def searchProductsByCar(self, args):
+    #TESTED:YES
+    def getCarMotor(self):
+        products = self.db
+        cm = []
+        for doc in products.distinct('car.motor'):
+            cm.append(doc)
+        return cm
 
-        dao = ProductsDao()
-        # TESTED: YES; works
-        if (len(args)==4):
-            cmake = args[0]
-            cmodel = args[1]
-            cyear = args[2]
-            cmotor = args[3]
-            plist = dao.getProductsByCar(cmake, cmodel, cyear, cmotor)
-            result_list = []
-            for row in plist:
-                result = self.products_dictionary(row)
-                result_list.append(result)
-            return result_list
-        # TESTED: YES; works
-        elif len(args)==3:
-            cmake = args[0]
-            cmodel = args[1]
-            cyear = args[2]
-            plist = dao.getProductByMakeModelYear(cmake,cmodel,cyear)
-            result_list = []
-            for row in plist:
-                result = self.products_dictionary(row)
-                result_list.append(result)
-            return result_list
+    # Returns the product information by searching the car make, car model,
+    # car year, and car motor
+    # author: Luis Perez
+    # tested:YES
+    def getProductsByCar(self, cmake, cmodel, cyear, cmotor):
+        products = self.db
+        p = []
+        for doc in products.find({'car.make': cmake, 'car.model': cmodel, 'car.year': cyear, 'car.motor': cmotor}):
+            p.append(doc)
+        return p
 
-        # TESTED: YES; works
-        elif(len(args)==2):
-            cmake = args[0]
-            cmodel = args[1]
+    # Returns the product information based on the make of the car, model, and year
+    # author: Luis Perez
+    # tested:YES
+    def getProductByMakeModelYear(self, cmake, cmodel, cyear):
+        products = self.db
+        p = []
+        for doc in products.find({'car.make': cmake, 'car.model': cmodel, 'car.year': cyear}):
+            p.append(doc)
+        return p
 
-            plist = dao.getProductByCarMakeModel(cmake, cmodel)
-            result_list = []
-            for row in plist:
-                result = self.products_dictionary(row)
-                result_list.append(result)
-            return result_list
-        # TESTED: YES
-        elif(len(args)==1):
-            cmake = args[0]
-            plist = dao.getProductByCarMake(cmake)
-            result_list = []
-            for row in plist:
-                result = self.products_dictionary(row)
-                result_list.append(result)
-            return result_list
+    #Returns the product information based on car make and model
+    # author: Luis Perez
+    # tested:YES
+    def getProductByCarMakeModel(self, cmake, cmodel):
+        products = self.db
+        p = []
+        for doc in products.find({'car.make': cmake, 'car.model': cmodel}):
+            p.append(doc)
+        return p
 
-    def getQuantityByIDAndLocation(self,pid,plocation):
+
+    def getProductByCarYear(self,cyear):
         """
-        Returns the quantity of the product by its id and location
-        author: Luis Perez
-        TESTED: YES WORKS
+         Returns the product information based on car year
+         author: Luis Perez
+        tested:NO
+        :param cyear: car year
+        :return: returns the product information by year of the car
+        """
+        products = self.db
+        p = []
+        for doc in products.find({'car.year':cyear}):
+            p.append(doc)
+        return p
+
+
+
+    def getProductByCarMake(self, cmake):
+        """
+
+        :param cmake:
+        :return:
+        """
+        products = self.db
+        p = []
+        for doc in products.find({'car.make': cmake}):
+            p.append(doc)
+        return p
+
+    def getInfoForCheckout(self, pid):
+        """
+        Returns the information when a user checks out in the shopping cart
+        Author: Luis Perez
         :param pid:
+        :return: Returns the
+        """
+        checkout = []
+        for doc in self.db.find({'pid': pid}, {'pid': 1, 'pname': 1, 'pprice': 1,'_id':0}):
+            checkout.append(doc)
+        return checkout
+
+    def productExistByMany(self,cmake,cmodel,cyear,cmotor,pid,plocation):
+        """
+
+        :param pid:
+        :param pname:
         :param plocation:
         :return:
         """
-        dao = ProductsDao()
+        products = self.db
+        p = products.findOne({'car.make': cmake,
+                                  'car.model': cmodel,
+                                  'car.year':cyear,
+                                  'car.motor':cmotor,
+                                  'pid':pid,
+                                  'plocation':plocation})
+        return p
 
-        return dao.getQtyByIDandLocation(pid,plocation)
+    def getQtyByIDandLocation(self, pid, plocation):
 
+        return self.db.find_one({'pid':pid,'plocation':plocation},{'qty':1,'_id':0})
 
-    def getInfoForCheckout(self,pid):
+    def productExistByID(self, pid):
         """
-
+        Returns if a product exist by its id
         :param pid:
         :return:
         TESTED: NO
         """
-        dao = ProductsDao()
-        checkoutList = dao.getInfoForCheckout(pid)
-        result_list = []
-        for row in checkoutList:
-            result = self.products_dictionary(row)
-            result_list.append(result)
-        return result_list
+        products = self.db
+        p = products.find_one({'pid': pid})
+        return p
 
-
-    def addProduct(self,image,cmake,cmodel,cyear,cmotor,pid,pcategory,pname,pdetails,plocation,pprice,pbrand,qty):
+    def productExistByIDAndLocation(self, pid,plocation):
         """
+
+        :param pid:
+        :return:
+        TESTED: YES
+        """
+        products = self.db
+        p = products.find_one({'pid': pid,'plocation':plocation})
+        return p
+
+
+    #################################################################################################################
+    #       INSERT PRODUCT
+    #################################################################################################################
+
+
+
+    def insertProduct(self,image,cmake,cmodel,cyear,cmotor,pid,pcategory,pname,pdetails,plocation,pprice,pbrand,qty):
+        """
+
          Inserts a new product to the database
          author: Luis Perez
          TESTED: NO
+
           :param image: image of the product if any
           :param cmake: car make
           :param cmodel: car model
@@ -208,119 +240,107 @@ class ProductsHandler:
           :param qty: quantity of the product in that specific location
 
          """
+        #car object
+        car = {
+            "make":cmake,
+            "model":cmodel,
+            "year":cyear,
+            "motor":cmotor
+        }
 
-        dao = ProductsDao()
+        product = {
+            "image":image,
+            "car":car,
+            "pid":pid,
+            "pcategory":pcategory,
+            "pname":pname,
+            "pdetails":pdetails,
+            "plocation":plocation,
+            "pprice":pprice,
+            "pbrand":pbrand,
+            "qty":qty
+        }
+        return self.db.insert_one(product).inserted_id
 
-        if dao.productExistByMany(cmake,cmodel,cyear,cmotor,pid,plocation):
-            return False
-        else:
-            dao.insertProduct(image,cmake,cmodel,cyear,cmotor,pid,pcategory,pname,pdetails,plocation,pprice,pbrand,qty)
-            return True
+    ################################################################################
+    #       UPDATE PRODUCT
+    ################################################################################
 
 
     def updateProductQtyByLocation(self, pid, plocation, qty):
         """
+        Updates the qty of the product by location and product id
+        :param pid: product id
+        :param plocation: location where the product is
+        :param qty: quantity update
+        :return:
+        TESTED: NO
+        """
+        return self.db.update({'pid': pid,'plocation':plocation}, {'$set': {'qty': qty}})
 
+    def updateProductById(self,image,cmake,cmodel,cyear,cmotor,pid,pcategory,pname,pdetails,plocation,pprice,pbrand,qty):
+        """
+        Updates the product by product ID
+        author: Luis Perez
+        :param image:
+        :param cmake:
+        :param cmodel:
+        :param cyear:
+        :param cmotor:
         :param pid:
+        :param pcategory:
+        :param pname:
+        :param pdetails:
         :param plocation:
+        :param pprice:
+        :param pbrand:
         :param qty:
         :return:
-        TESTED: NO
+        TESTED NO
         """
-        dao = ProductsDao()
-        p = dao.productExistByIDAndLocation(pid,plocation)
-        try:
-            if p['pid'] == pid:
-                try:
-                    pass
-                except:
-                    pass
-        except:
-            pass
+        car = {
+            "make": cmake,
+            "model": cmodel,
+            "year": cyear,
+            "motor": cmotor
+        }
 
-    def updateProductQtyInEveryLocation(self,pid,qty):
-        """
+        product = {
+            "image": image,
+            "car": car,
+            "pid": pid,
+            "pcategory": pcategory,
+            "pname": pname,
+            "pdetails": pdetails,
+            "plocation": plocation,
+            "pprice": pprice,
+            "pbrand": pbrand,
+            "qty": qty
+        }
 
-        :param pid:
-        :param qty:
-        :return:
-        TESTED: NO
-        """
-        dao = ProductsDao()
-        p = dao.productExistByID(pid)
-        try:
-            if p['pid'] == pid:
-                try:
-                    pass
-                except:
-                    pass
-        except:
-            pass
-
-    def updateProductImage(self,pid):
-        """
-
-        :param pid:
-        :return:
-        TESTED: NO
-        """
-        dao = ProductsDao()
-        p = dao.productExistByID(pid)
-        try:
-            if p['pid'] == pid:
-                try:
-                    pass
-                except:
-                    pass
-        except:
-            pass
-
-    def updateAllProductAttributes(self,image,cmake,cmodel,cyear,cmotor,pid,pcategory,pname,pdetails,plocation,pprice,pbrand,qty):
-        pass
+        return self.db.update({'pid':pid},{'$set':{'product':product}})
 
     def deleteProductByID(self,pid):
         """
-        Deletes the product by its id. First it verifies that it exist
-
+        Deletes the product by its id
         :param pid:
         :return:
-        TESTED: YES
+        TESTED NO
         """
-        dao = ProductsDao()
-        p = dao.productExistByID(pid)
-        try:
-            if p['pid'] == pid:
-                try:
-                    dao.deleteProductByID(pid)
-                    return True
-                except:
-                    return False
-
-
-        except:
-            return False
+        return self.db.delete_many({"pid": pid})
 
     def deleteProductByIDAndLocation(self,pid,plocation):
         """
-
+        Deletes the product in the specified location
         :param pid:
         :param plocation:
         :return:
         TESTED: YES
         """
 
-        dao = ProductsDao()
-        p =  dao.productExistByIDAndLocation(pid,plocation)
-        try:
-            if p['pid'] == pid and p['plocation']==plocation:
+        return self.db.delete_many({'pid':pid,'plocation':plocation})
 
-                try:
-                    dao.deleteProductByIDAndLocation(pid,plocation)
-                    return True
-                except:
-                    return False
-        except:
-            return False
+
 
 
 
