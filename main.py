56a30c88147ddfa708e0ea3b4a22127582856de9
jcdfusion <jaime.cortes@upr.edu>
2018-04-26 20:49:58 -0400

    User Tester Updated

--- main.py
+++ main.py
@@ -87,6 +87,19 @@ def adminStaff():
             None
     return render_template('adminStaff.html', staff=staff)
 
+# === ERROR HANDLING === #
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
