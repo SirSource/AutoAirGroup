a3ffffa1d7f67858e72d9bad126b61ceda587597
Luis <luis@luis.perez67@upr.edu>
2018-05-20 16:21:03 -0400

    Generic Product search can now search by car, year, model, or motor. MUST BE TESTED

--- utilities/sendmail.py
+++ utilities/sendmail.py
@@ -26,8 +26,8 @@ class SendMail:
     def sendAccountCreationEmail(self, userEmail):
         to_email = Email(userEmail)
         subject = "!Bienvenido a Auto Air Group!"
-        content = Content("text/plain",
-                          "<h1>!Gracias por crear una cuenta con Auto Air Group!</h1><p>Desde su cuenta puede ver el historial de compras y tener un récord para sus recibos.</p>")
+        content = Content("text/html",
+                          "<html><h1>!Gracias por crear una cuenta con Auto Air Group!</h1> <h3>Desde su cuenta puede ver el historial de compras y tener un récord para sus recibos.</h3></html>")
         mail = Mail(self.from_email, subject, to_email, content)
         response = self.sg.client.mail.send.post(request_body=mail.get())
         return response
@@ -35,11 +35,12 @@ class SendMail:
     def sendAccountRecoverEmail(self, userEmail, code):
         to_email = Email(userEmail)
         subject = "Recuperar Acceso a su Cuenta"
-        content = Content("text/plain",
-                          'Para recuperar acceso a su cuenta haga clic en el enlace: <a href="https://autoair.io/user/reset/password/">Recuperar acceso<a>' + str(
-                              code))
+        content = Content("text/html",
+                          'Para recuperar acceso a su cuenta haga clic en el enlace: <html>https://autoair.io/user/reset/password/'+str(
+                              code)+'</html> Recuperar acceso')
         mail = Mail(self.from_email, subject, to_email, content)
         response = self.sg.client.mail.send.post(request_body=mail.get())
+        print("recovery pass")
         return response
 
     def sendOrderConfirmationEmail(self, userEmail, orderNum, total):
@@ -49,12 +50,17 @@ class SendMail:
                           "Gracias por comprar en Auto Air Group. Su número de orden es: " + str(orderNum) + ".")
         mail = Mail(self.from_email, subject, to_email, content)
         response = self.sg.client.mail.send.post(request_body=mail.get())
+
         return response
 
     def sendChangePasswordLink(self, userEmail, link):
+        print("level 1")
         to_email = Email("%s" % userEmail)
+        print("level 2")
         subject = "Cambio de contrasena"
-        content = Content("text/plain", "Favor de ir al siguiente enlace para cambiar de contrasena %s " % link)
+        content = Content("text/html", "Favor de ir al siguiente enlace para cambiar de contrasena %s " '<html>' % link +'</html>')
+        print("level 3")
         mail = Mail(self.from_email, subject, to_email, content)
         response = self.sg.client.mail.send.post(request_body=mail.get())
+        print("password")
         return response
